737a3136591e4af60c31a3c415883c4675457e18
Saurav Gupta <69887-saurav641@users.noreply.drupalcode.org>
2025-04-30 22:25:55 +0530

    cors error

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import router from './routes';
 
 dotenv.config();
 
 const app = express();
+
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://your-frontend-domain.com'], 
+  methods: ['GET', 'POST', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+}));
+
 app.use(express.json());
 app.use(router);
 
